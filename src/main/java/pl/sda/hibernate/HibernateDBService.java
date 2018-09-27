e62154c7ce34a3ab9fe9e491821757fdf5e5a732
wozog <hugowojtek@o2.pl>
2018-09-27 19:44:40 +0200

    hibernate+console odczyt z bazy ok zapis nok

--- src/main/java/pl/sda/hibernate/HibernateDBService.java
+++ src/main/java/pl/sda/hibernate/HibernateDBService.java
@@ -2,10 +2,12 @@ package pl.sda.hibernate;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import pl.sda.jpa.model.Lekarze;
+import pl.sda.jpa.model.*;
 
 import javax.transaction.Transactional;
-import java.util.List;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.*;
 
 public class HibernateDBService {
 
@@ -16,18 +18,257 @@ public class HibernateDBService {
         instance = HbnConfig.getInstance();
         session = instance.openSession();
     }
-//    @Transactional
-    public Lekarze getLekarz() {
-        Lekarze lekarz = session.find(Lekarze.class, 5L);
-        return lekarz;
-    }
-
-//    @Transactional
+    @Transactional
     public void getJpaDBallDoctors() {
         String jpql = "SELECT l FROM Lekarze l ORDER BY l.nazwisko";
         List<Lekarze> lekarze = session.createQuery(jpql).getResultList();
         for (Lekarze l : lekarze) {
             System.out.println(l.getId() + "," + l.getImie() + "," + l.getNazwisko());
         }
+    }
+
+    @Transactional
+    public void getJpaDBallDoctorsAndSpecial() {
+
+        String jpql = "SELECT l FROM Lekarze l ORDER BY l.nazwisko";
+        List<Lekarze> lekarze = session.createQuery(jpql).getResultList();
+        for (Lekarze l : lekarze) {
+            System.out.println(l);
+        }
+    }
+
+    @Transactional
+    public void insertJpaDBnewDoctor(Lekarze lekarz) {
+
+        session.persist(lekarz);
+
+    }
+
+    @Transactional
+    public Wojewodztwa getJpaDBDistrictByIdNumber(Long district) {
+        Wojewodztwa wojewodztwa;
+
+        wojewodztwa = session.find(Wojewodztwa.class, district);
+        return wojewodztwa;
+    }
+
+    @Transactional
+    public Specjalizacje getJpaDBSpecializationByIdNumber(Long spec) {
+        Specjalizacje specjalizacje;
+
+        specjalizacje = session.find(Specjalizacje.class, spec);
+        return specjalizacje;
+    }
+
+    @Transactional
+    public void removeJpaDBdoctor(Long id) {
+
+        Lekarze lekarze = session.find(Lekarze.class, id);
+        session.remove(lekarze);
+    }
+
+    @Transactional
+    public void getJpaDBdoctorDetails(long id2) {
+
+        Lekarze lekarze = session.find(Lekarze.class, id2);
+        System.out.println(lekarze);
+    }
+
+    @Transactional
+    public void getJpaDBallPatients() {
+
+        String jpql = "SELECT p FROM Pacjenci p ORDER BY p.id";
+        List<Pacjenci> pacjenci = session.createQuery(jpql).getResultList();
+        for (Pacjenci p : pacjenci) {
+            System.out.println("id_pacjenta:" + p.getId() + ",imie:" + p.getImie() + ",nazwisko:" + p.getNazwisko() + ",pesel:" + p.getPesel());
+        }
+
+    }
+
+    @Transactional
+    public void getJpaDBVisitsWithDate() {
+        String jpql = "SELECT w FROM Wizyty w ORDER BY w.id";
+        List<Wizyty> wizyty = session.createQuery(jpql).getResultList();
+        for (Wizyty w : wizyty) {
+            System.out.println("-id_wizyty:" + w.getLd() + "-data_wizyty:" + w.getDataWizyty() + "-dane_lekarza:" + w.getLekarz() + "-dane_pacjenta:" + w.getPacjent() + "-cena:" + w.getAktualnaCenaWizyty());
+        }
+    }
+
+    @Transactional
+    public void getJpaDBpatientDetails(Long id3) {
+        Pacjenci pacjent = session.find(Pacjenci.class, id3);
+        System.out.println(pacjent);
+    }
+
+    @Transactional
+    public void insertJpaDBnewPatient(Pacjenci pacjent) {
+
+        session.persist(pacjent);
+
+    }
+
+    @Transactional
+    public void removeJpaDBpatient(long id4) {
+
+        Pacjenci pacjent = session.find(Pacjenci.class, id4);
+        session.remove(pacjent);
+    }
+
+    public boolean insertJpaDBnewVisit(long idDoctor, long idPatient, LocalDateTime dateVisit) {
+
+        String jpql = "SELECT w FROM Wizyty w WHERE w.lekarz=?1";
+
+        Lekarze lekarz = session.find(Lekarze.class, idDoctor);
+
+        if (lekarz==null) {
+            System.out.println("Brak tego lekarza wybierz innego");
+            return false;
+        }
+
+        List<Wizyty> list = session.createQuery(jpql).setParameter(1, lekarz).getResultList();
+
+        Set<String> set = null;
+        Map<String, Set<String>> map = new TreeMap<String, Set<String>>();
+
+        for (Wizyty w : list) {
+            String visit = String.valueOf(w.getDataWizyty());
+            String date = visit.substring(0, 10);
+            String time = visit.substring(11);
+
+            set = new TreeSet<String>();
+            boolean mark = false;
+
+            for (Map.Entry m : map.entrySet()) {
+                if (date.equals(m.getKey())) {
+
+                    Set<String> set2 = (Set) m.getValue();
+                    set2.add(time);
+                    m.setValue(set2);
+                    mark = true;
+                    break;
+                }
+            }
+
+            if (!(mark)) {
+                set.add(time);
+                map.put(date, set);
+            }
+        }
+        String dateVisit2 = String.valueOf(dateVisit);
+        String dateForCheck = dateVisit2.substring(0, 10);
+        String timeForCheck = dateVisit2.substring(11);
+        Set<String> set3 = null;
+        if (map.containsKey(dateForCheck)) {
+            set3 = map.get(dateForCheck);
+            if (set3.contains(timeForCheck)) {
+                //System.out.println("termin zajęty");
+                return false;
+            } else {
+                //System.out.println("wolny termin wiec rezerwujemy");
+                set3.add(timeForCheck);
+                map.put(dateForCheck, set3);
+            }
+        } else {
+            //System.out.println("wolny termin wiec rezerwujemy");
+            set3 = new TreeSet<String>();
+            set3.add(timeForCheck);
+            map.put(dateForCheck, set3);
+        }
+
+        Wizyty wizyta = new Wizyty();
+
+        wizyta.setLekarz(lekarz);
+
+        Pacjenci pacjent = session.find(Pacjenci.class, idPatient);
+        wizyta.setPacjent(pacjent);
+
+        wizyta.setAktualnaCenaWizyty(new BigDecimal(0.0));
+
+        wizyta.setDataUmowienia(LocalDateTime.now());
+
+        wizyta.setDataWizyty(dateVisit);
+
+        session.persist(wizyta);
+
+        return true;
+    }
+
+    public void removeJpaDBvisit(long idVisit) {
+
+        Wizyty wizyta = session.find(Wizyty.class, idVisit);
+        session.remove(wizyta);
+
+    }
+
+    public void getJpaDBvisitsAllForDoctorWithDate(long idDoctor2) {
+
+        String jpql = "SELECT w FROM Wizyty w WHERE w.lekarz=?1";
+
+        Lekarze lekarz = session.find(Lekarze.class, idDoctor2);
+        List<Wizyty> list = session.createQuery(jpql).setParameter(1, lekarz).getResultList();
+
+        Set<String> set = null;
+        Map<String, Set<String>> map = new TreeMap<String, Set<String>>();
+
+        for (Wizyty w : list) {
+            String visit = String.valueOf(w.getDataWizyty());
+            String date = visit.substring(0, 10);
+            String time = visit.substring(11);
+
+            set = new TreeSet<String>();
+            boolean mark = false;
+
+            for (Map.Entry m : map.entrySet()) {
+                if (date.equals(m.getKey())) {
+
+                    Set<String> set2 = (Set) m.getValue();
+                    set2.add(time);
+                    m.setValue(set2);
+                    mark = true;
+                    break;
+                }
+            }
+
+            if (!(mark)) {
+                set.add(time);
+                map.put(date, set);
+            }
+        }
+
+        for (Map.Entry m : map.entrySet()) {
+            System.out.print(m.getKey().toString());
+            System.out.print(" + ");
+            for (Object s : (Set) m.getValue()) {
+                System.out.print(s);
+                System.out.print(" | ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void getJpaDBvisitsAllForEveryDoctorsWithDate() {
+
+        String jpql = "SELECT l.id FROM Lekarze l ORDER BY l.id";
+
+        List<Long> list = session.createQuery(jpql).getResultList();
+
+        for (Long l : list) {
+            getJpaDBdoctorDetails(l);
+            getJpaDBvisitsAllForDoctorWithDate(l);
+        }
+    }
+
+    public void getDBvisitsAllForPatient(long idPatient2) {
+
+        Pacjenci pacjent = session.find(Pacjenci.class,idPatient2);
+
+        String jpql = "SELECT w FROM Wizyty w WHERE pacjent=?1";
+
+        List<Wizyty> list = session.createQuery(jpql).setParameter(1,pacjent).getResultList();
+
+        for (Wizyty w:list) {
+            System.out.println("id_wizyty:"+w.getLd()+"-data_wizyty:"+w.getDataWizyty()+"-dane_lekarza:"+w.getLekarz());
+        }
+
     }
 }
--- src/main/java/pl/sda/hibernate/Main.java
+++ src/main/java/pl/sda/hibernate/Main.java
@@ -1,28 +1,347 @@
 package pl.sda.hibernate;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import pl.sda.jpa.JpaDBService;
+
 import pl.sda.jpa.model.Lekarze;
+import pl.sda.jpa.model.Pacjenci;
+import pl.sda.jpa.model.Specjalizacje;
+import pl.sda.jpa.model.Wojewodztwa;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
 
 public class Main {
 
     private static HibernateDBService hibernateDBService = null;
+    private static Scanner scanner = null;
 
     public Main(){
         hibernateDBService = new HibernateDBService();
+        scanner = new Scanner(System.in);
     }
 
     public static void main(String...strings){
 
         new Main();
 
-        Lekarze lekarze = hibernateDBService.getLekarz();
-        System.out.println("one doctor----------------------------");
-        System.out.println(lekarze);
-        System.out.println("all doctors----------------------------");
-        hibernateDBService.getJpaDBallDoctors();
+        while(true) {
+            showMenu();
+            choice();
+        }
+    }
+
+    private static void choice() {
+        Integer number = scanner.nextInt();
+
+        switch (number) {
+            case 1:
+
+                hibernateDBService.getJpaDBallDoctorsAndSpecial();
+                break;
+
+            case 2:
+
+                Lekarze lekarz = new Lekarze();
+                lekarz = enterNewDoctor(lekarz);
+                hibernateDBService.insertJpaDBnewDoctor(lekarz);
+                break;
+
+            case 3:
+
+                hibernateDBService.getJpaDBallDoctors();
+                long id = removeDoctor();
+                hibernateDBService.removeJpaDBdoctor(id);
+                break;
+
+            case 4:
+
+                hibernateDBService.getJpaDBallDoctors();
+                long id2 = getDoctor();
+                hibernateDBService.getJpaDBdoctorDetails(id2);
+
+            case 5:
+
+                hibernateDBService.getJpaDBallPatients();
+                break;
+
+            case 6:
+                hibernateDBService.getJpaDBVisitsWithDate();
+                break;
+
+            case 7:
+
+                hibernateDBService.getJpaDBallPatients();
+                Long id3 = getPatient();
+                hibernateDBService.getJpaDBpatientDetails(id3);
+                break;
 
 
+            case 8:
+
+                Pacjenci pacjent = new Pacjenci();
+                pacjent = enterNewPatient(pacjent);
+                hibernateDBService.insertJpaDBnewPatient(pacjent);
+                break;
+
+            case 9:
+
+                hibernateDBService.getJpaDBallPatients();
+                long id4 = removePatient();
+                hibernateDBService.removeJpaDBpatient(id4);
+                break;
+
+
+            case 11:
+
+                hibernateDBService.getJpaDBallDoctors();
+                long idDoctor = setNewVisitForDoctor();
+                hibernateDBService.getJpaDBallPatients();
+                long idPatient = setNewVisitForPatient();
+                LocalDateTime dateVisit = setNewVisitDate();
+                if (hibernateDBService.insertJpaDBnewVisit(idDoctor, idPatient, dateVisit)) {
+                    System.out.println("wolny termin wiec rezerwujemy");
+                }
+                else {
+                    System.out.println("termin zajęty - wybierz inny");
+                }
+
+                break;
+
+            case 12:
+
+                hibernateDBService.getJpaDBVisitsWithDate();
+                long idVisit = removeVisit();
+                hibernateDBService.removeJpaDBvisit(idVisit);
+                break;
+
+            case 13:
+
+                hibernateDBService.getJpaDBallDoctors();
+                long idDoctor2 = schowVisitForDoctor();
+                hibernateDBService.getJpaDBvisitsAllForDoctorWithDate(idDoctor2);
+                break;
+
+            case 14:
+
+                hibernateDBService.getJpaDBvisitsAllForEveryDoctorsWithDate();
+                break;
+
+            case 15:
+                hibernateDBService.getJpaDBallPatients();
+                long idPatient2 = showVisitForPatient();
+                hibernateDBService.getDBvisitsAllForPatient(idPatient2);
+                break;
+
+            case 0:
+                System.out.println("kończe.....");
+                System.exit(0);
+
+            default:
+        }
+    }
+
+    private static long showVisitForPatient() {
+        System.out.println("podaj numer id pacjenta, którego wizyty chcesz zobaczyć");
+        return scanner.nextLong();
+    }
+
+    private static long schowVisitForDoctor() {
+        System.out.println("podaj numer id lekarza, którego wizyty chcesz zobaczyć");
+        return scanner.nextLong();
+    }
+
+    private static long removeVisit() {
+        System.out.println("podaj numer id wizyty do usunięcia");
+        return scanner.nextLong();
+    }
+
+    private static LocalDateTime setNewVisitDate() {
+        System.out.println("podaj date wizyty format YYYY-MM-ddThh-mm");
+        String text = scanner.next();
+        DateTimeFormatter dateTimeFormatter = ISO_LOCAL_DATE_TIME;
+        LocalDateTime date = LocalDateTime.parse(text,dateTimeFormatter);
+        return date;
+    }
+
+    private static long setNewVisitForPatient() {
+        System.out.println("podaj numer id pacjenta, dla którego chcesz zamówić wizytę");
+        return scanner.nextLong();
+    }
+
+    private static long setNewVisitForDoctor() {
+        System.out.println("podaj numer id lekarza, do którego chcesz zamówić wizytę");
+        return scanner.nextLong();
+    }
+
+    private static long getPatient() {
+        System.out.println("podaj numer pacjenta, którego chcesz szczegółowo zobaczyć");
+        Long id = scanner.nextLong();
+        return id;
+
+    }
+
+    private static long getDoctor() {
+        System.out.println("podaj numer lekarza, którego chcesz szczegółowo zobaczyć");
+        long id = scanner.nextLong();
+        return id;
+
+    }
+
+    private static long removeDoctor() {
+        System.out.println("podaj numer lekarza, którego chcesz usunąć");
+        long id = scanner.nextLong();
+        return id;
+
+    }
+
+    private static long removePatient() {
+        System.out.println("podaj numer pacjenta, którego chcesz usunąć");
+        long id = scanner.nextLong();
+        return id;
+
+    }
+
+    private static Pacjenci enterNewPatient(Pacjenci pacjent) {
+
+        System.out.println("podaj imię pacjenta");
+        String name = scanner.next();
+        pacjent.setImie(name);
+
+        System.out.println("podaj nazwisko pacjenta");
+        String surname = scanner.next();
+        pacjent.setNazwisko(surname);
+
+        System.out.println("podaj pesel pacjenta");
+        String pesel = scanner.next();
+        pacjent.setPesel(pesel);
+
+        System.out.println("podaj datę urodzenia pacjenta");
+        String dateBirth = scanner.next();
+        Date date = Date.valueOf(dateBirth);
+        pacjent.setDataUrodzenia(date);
+
+        System.out.println("podaj płeć pacjenta");
+        String sex = scanner.next();
+        pacjent.setPlec(sex);
+
+        System.out.println("podaj miasto zamieszkania pacjenta");
+        String city = scanner.next();
+        pacjent.setMiasto(city);
+
+        System.out.println("podaj ulicę zamieszkania pacjenta");
+        String street = scanner.next();
+        pacjent.setUlica(street);
+
+        System.out.println("podaj numer domu/lokalu pacjenta ");
+        String numberHouse = scanner.next();
+        pacjent.setNrLocalu(numberHouse);
+
+        System.out.println("podaj województwo");
+
+        System.out.println("1-dolnośląskie");
+        System.out.println("2-kujawsko-pomorskie");
+        System.out.println("3-lubelskie");
+        System.out.println("4-lubuskie");
+
+        System.out.println("5-łódzkie");
+        System.out.println("6-małopolskie");
+        System.out.println("7-mazowieckie");
+        System.out.println("8-opolskie");
+
+        System.out.println("9-podkarpackie");
+        System.out.println("10-podlaskie");
+        System.out.println("11-pomorskie");
+        System.out.println("12-śląskie");
+
+        System.out.println("13-świętokrzyskie");
+        System.out.println("14-warmińsko-mazurskie");
+        System.out.println("15-wielkopolskie");
+        System.out.println("16-zachodniopomorskie");
+
+        Long spec = scanner.nextLong();
+
+        Wojewodztwa wojewodztwa;
+        wojewodztwa = hibernateDBService.getJpaDBDistrictByIdNumber(spec);
+        pacjent.setWojewodztwo(wojewodztwa);
+
+        return pacjent;
+    }
+
+    private static Lekarze enterNewDoctor(Lekarze lekarz) {
+
+        System.out.println("podaj imię lekarza");
+        String name = scanner.next();
+        lekarz.setImie(name);
+
+        System.out.println("podaj nazwisko lekarza");
+        String surname = scanner.next();
+        lekarz.setNazwisko(surname);
+
+        System.out.println("podaj specjalizację");
+        System.out.println("1-kardiolog");
+        System.out.println("2-laryngolog");
+        System.out.println("3-neurolog");
+        System.out.println("4-internista");
+        System.out.println("5-okulista");
+        System.out.println("6-chirurg");
+        System.out.println("7-psychiatra");
+        Long spec = scanner.nextLong();
+
+        Specjalizacje specjalizacje;
+        specjalizacje = hibernateDBService.getJpaDBSpecializationByIdNumber(spec);
+
+        lekarz.setSpecjalizacja(specjalizacje);
+
+        System.out.println("podaj numer uprawnień");
+        String nr_perm = scanner.next();
+        lekarz.setNrUprawnien(nr_perm);
+
+        System.out.println("podaj numer gabinetu");
+        Integer nr_office = scanner.nextInt();
+        lekarz.setNrGabinetu(nr_office);
+
+        System.out.println("podaj numer telefonu");
+        String nr_phone = scanner.next();
+        lekarz.setNrTelefonu(nr_phone);
+
+        System.out.println("podaj email");
+        String email = scanner.next();
+        lekarz.setEmail(email);
+
+        System.out.println("podaj cenę wizyty");
+        String price_visit = scanner.next();
+        BigDecimal bigDecimal = new BigDecimal(price_visit);
+        lekarz.setCenaWizyty(bigDecimal);
+
+        return lekarz;
+    }
+
+
+    private static void showMenu() {
+        System.out.println("**************************");
+        System.out.println("***Przychodnia lekarska***");
+        System.out.println("1-lista dostępnych lekarzy");
+        System.out.println("2-dodaj nowego lekarza");
+        System.out.println("3-usuń istniejacego lekarza");
+        System.out.println("4-wyświetl lekarza szczegółowo");
+        System.out.println("5-lista dostępnych pacjentów");
+        System.out.println("6-lista zarezerwowanych wizyt");
+        System.out.println("7-wyświetl pacjenta szczegółowo");
+        System.out.println("8-dodaj nowego pacjenta");
+        System.out.println("9-usuń istniejacego pacjenta");
+
+        System.out.println("11-dodaj nowy termin wizyty");
+        System.out.println("12-usuń stary termin wizyty");
+        System.out.println("13-wizyty danego lekarza");
+        System.out.println("14-wizyty wszystkich lekarzy");
+        System.out.println("15-wizyty danego pacjenta");
+
+        System.out.println("0-zakończ program");
+        System.out.println("?");
     }
 }
